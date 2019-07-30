b47388d205e0d76c3b5f53a74e59b3f333a6bae2
Egor Kirsanov <kirsanov@papajohns.ru>
2019-07-30 19:45:29 +0300

    babel with webpack

--- src/main.js
+++ src/main.js
@@ -2,6 +2,23 @@ require("./main.css");
 require("./index.html");
 console.log("heeey1");
 
-const a = () => console.log("---");
+const funcWithPromise = (name = "one") => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`${name}_project`);
+    }, 1700);
+  });
+};
 
-a();
+async function asyncFunc(name) {
+  try {
+    const res = await funcWithPromise(name);
+    console.log(res);
+
+    await console.log("---");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+asyncFunc("_one");
